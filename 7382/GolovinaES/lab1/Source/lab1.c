6cb27cbb01688e5158dd4faee27861f873a5dbbc
kate31 <katy.s.golovina@gmail.com>
2018-09-27 23:33:40 +0300

    VARIABLES RENAMED

--- 7382/GolovinaES/lab1/Source/lab1.c
+++ 7382/GolovinaES/lab1/Source/lab1.c
@@ -13,9 +13,9 @@ int main(){
 	int index=0;		//current index of symbol in string, which is checking/going to check
 	int result;			// result of program, is string "list of parametrs" or not
 	long int length=100;		//length of the string
-	char * s;			//pointer to the string
-	char l[20];			//using to read length as string
-	FILE *f=NULL;			//pointer to the text file
+	char * string;			//pointer to the string
+	char length_str[20];			//using to read length as string
+	FILE *file=NULL;			//pointer to the text file
 
 	//check if person want to type text in terminal or use <file>.txt
 	fputs("If you want to type input in terminal  - type 0.\nIf you want to use input from file - type 1.\n",stdout);
@@ -26,32 +26,32 @@ int main(){
 		fputs("Input from file is on.\n",stdout);
 		fputs("Please, type the name of the file in format <name>.txt\n",stdout);
 		scanf("\n");
-		fgets(l,20,stdin);
-		l[strlen(l)-1]='\0';
-		strcat(name,l);
-		f = fopen(name,"r");
+		fgets(length_str,20,stdin);
+		length_str[strlen(length_str)-1]='\0';
+		strcat(name,length_str);
+		file = fopen(name,"r");
 		
 		//check if pointer to the file isn't NULL
-		if (!f){
+		if (!file){
 			fputs("Error in opening file. There is no such file in directory or name of it was written incorrectly.\nPlease, try again.\n",stdout);	
 		//	scanf("\n");
-			fgets(l,20,stdin);
-			l[strlen(l)-1]='\0';
+			fgets(length_str,20,stdin);
+			length_str[strlen(length_str)-1]='\0';
 			strcpy(name,"./\0");
-			strcat(name,l);
-			f = fopen(name,"r");
+			strcat(name,length_str);
+			file = fopen(name,"r");
 		}
 
 		//check if pointer to the file isn't NULL - 2 time
-		if (!f){
+		if (!file){
 			fputs("Error in opening file.\n",stdout);
 			input_format=0;
 		}
 
 		//if everything correct - read maximum length and string from <file>.txt
 		if (input_format==1){
-			fgets(l,20,f);
-			length=strtol(l,&s,10);
+			fgets(length_str,20,file);
+			length=strtol(length_str,&string,10);
 
 			//if data is written incorrectly in file - termination of the program
 			if (length<0 || length>SHRT_MAX || !length){
@@ -60,10 +60,10 @@ int main(){
 			}
 
 			//if data is correct - allocation memory and reading string from file
-			s=(char*)malloc(sizeof(char)*length);
-			fgets(s,length,f);
+			string=(char*)malloc(sizeof(char)*length);
+			fgets(string,length,file);
 		}
-		fclose(f);
+		fclose(file);
 	}
 
 	// Terminal mode, reading length of the string (and checking if it correct) and string
@@ -74,8 +74,8 @@ int main(){
 
 		//Check if maximum length is number and it's value in between 0 and 32767
 		while (1){
-			fgets(l,20,stdin);	//read length as string
-			length=strtol(l,&s,10); //transformation string to integer; length=0, if it fail to transformate
+			fgets(length_str,20,stdin);	//read length as string
+			length=strtol(length_str,&string,10); //transformation string to integer; length=0, if it fail to transformate
 			if (!length)
 				fputs("Incorrect data. Please, type number: \n\0",stdout);
 			else{
@@ -88,35 +88,35 @@ int main(){
 		}
 	
 		//allocates memory for string accordiing to the maximum length of it
-		s=(char*)malloc(sizeof(char)*length);
+		string=(char*)malloc(sizeof(char)*length);
 		fputs("Please, type the string:\n\0",stdout);
 		scanf("\n");
-		fgets(s,length,stdin);
+		fgets(string,length,stdin);
 	}
 
 	//creating of file sequence.txt, where will be the information about what function (recursive) was on
-	f= fopen("./sequence.txt","w+");
+	file= fopen("./sequence.txt","w+");
 
 	//deleting sign of next line
-	if (s[strlen(s)-1]=='\n') //maybe unnecessarily check
-			s[strlen(s)-1]='\0';
+	if (string[strlen(string)-1]=='\n') //maybe unnecessarily check
+			string[strlen(string)-1]='\0';
 
 	//start of recursive functions
-	result=list_of_parametrs(s,&index,&f);
-	fclose(f);
+	result=list_of_parametrs(string,&index,&file);
+	fclose(file);
 
 	//output of the string, green - correct part, red - incorrect
 	printf("\nINPUT STRING:\n");
 	for (int i=0;i<index;i++)
-		printf(ANSI_COLOR_GREEN "%c" ANSI_COLOR_RESET,s[i]);
-	for (int i=index;i<strlen(s);i++)
-		printf(ANSI_COLOR_RED "%c" ANSI_COLOR_RESET, s[i]);
+		printf(ANSI_COLOR_GREEN "%c" ANSI_COLOR_RESET,string[i]);
+	for (int i=index;i<strlen(string);i++)
+		printf(ANSI_COLOR_RED "%c" ANSI_COLOR_RESET, string[i]);
 	printf("\n");
 
 	//output the depth of recursion
 	recursion_depth(result);
 	remove("sequence.txt");
-free(s);
+free(string);
 
 return 0;
 }
